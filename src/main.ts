6bf814db597166f1382a96abca0be4bba38cfa6a
roll <eskarev@gmail.com>
2021-05-10 14:46:17 +0200

    v0.1.7

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
 
 async function run() {
   try {
     const myInput = core.getInput('myInput')
-    core.debug(`Hello ${myInput} from inside a container`)
-
-    // Get github context data
-    const context = github.context
-    console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
+    console.log(`Hello "${myInput}" from inside a container`)
   } catch (error) {
     core.setFailed(error.message)
   }
