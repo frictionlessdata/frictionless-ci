a9fd8edd205b26b28386bdb77d2f6910fe7d5b85
roll <eskarev@gmail.com>
2021-05-12 18:46:33 +0200

    Drafted report page

--- portal/src/pages/report/index.js
+++ portal/src/pages/report/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Layout from '@theme/Layout'
+import Head from '@docusaurus/Head'
+import useBaseUrl from '@docusaurus/useBaseUrl'
+import Safe from 'react-safe'
 
 function Report() {
   return (
     <Layout title="Report" description="Report renderer">
-      <div className="container margin-vert--lg">Report!</div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="//unpkg.com/frictionless-components/dist/frictionless-components.css"
+        />
+        <script src="https://unpkg.com/jszip/dist/jszip.js"></script>
+        <script src="//unpkg.com/frictionless-components@0.1.14/dist/frictionless-components.js"></script>
+      </Head>
+      <div className="container margin-vert--lg">
+        <h1>frictionlessdata/repository</h1>
+        <div id="report"></div>
+      </div>
+      <Safe.script src={useBaseUrl('js/report.js')}></Safe.script>
     </Layout>
   )
 }
---
+++ portal/static/js/report.js
@@ -0,0 +1,63 @@
+// TODO: isolate namespace
+async function main() {
+  const user = 'frictionlessdata'
+  const repo = 'repository'
+  const flow = 'frictionless'
+
+  // Get runId
+  const runs = await getData(`/repos/${user}/${repo}/actions/workflows/${flow}.yaml/runs`)
+  const runId = runs.workflow_runs[0].id
+  console.log(runId)
+
+  // Get artifactId
+  const artifacts = await getData(
+    `/repos/${user}/${repo}/actions/runs/${runId}/artifacts`
+  )
+  const artifactId = artifacts.artifacts[0].id
+  console.log(artifactId)
+
+  // Get zipFile
+  const zipFile = await getFile(
+    `/repos/${user}/${repo}/actions/artifacts/${artifactId}/zip`
+  )
+  console.log(zipFile)
+
+  // Get report
+  const archive = await JSZip.loadAsync(zipFile)
+  const contents = await archive.file('report.json').async('string')
+  const report = JSON.parse(contents)
+  console.log(report)
+
+  // Show report
+  // TODO: add H1 user/repo, link, etc
+  const element = document.getElementById('report')
+  frictionlessComponents.render(frictionlessComponents.Report, { report }, element)
+}
+
+async function getData(path) {
+  const TOKEN = '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'
+  const res = await fetch(`https://api.github.com${path}`, {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${TOKEN}`,
+    },
+  })
+  const data = await res.json()
+  return data
+}
+
+async function getFile(path) {
+  const TOKEN = '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'
+  const res = await fetch(`https://api.github.com${path}`, {
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${TOKEN}`,
+    },
+  })
+  const buffer = await res.blob()
+  return buffer
+}
+
+main()
+  .then(() => {})
+  .catch((error) => console.log(error))
