72ea1603d182d033c6a70d56fc06cf5e1b789124
roll <eskarev@gmail.com>
2021-05-12 17:47:59 +0200

    Bootstrapped report page

---
+++ portal/src/pages/report/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '@theme/Layout'
+
+function Report() {
+  return (
+    <Layout title="Report" description="Report renderer">
+      <div className="container margin-vert--lg">Report!</div>
+    </Layout>
+  )
+}
+
+export default Report
