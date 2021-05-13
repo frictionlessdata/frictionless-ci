27abd3b3ed2485afe78d27613df47f8d8935c45f
roll <eskarev@gmail.com>
2021-05-13 10:49:00 +0200

    Updated report page

--- portal/src/pages/report/index.js
+++ portal/src/pages/report/index.js
@@ -10,18 +10,45 @@ function Report() {
       <Head>
         <link
           rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
           href="//unpkg.com/frictionless-components/dist/frictionless-components.css"
         />
         <script src="https://unpkg.com/jszip/dist/jszip.js"></script>
-        <script src="//unpkg.com/frictionless-components@0.1.14/dist/frictionless-components.js"></script>
+        <script src="//unpkg.com/frictionless-components/dist/frictionless-components.js"></script>
       </Head>
       <div className="container margin-vert--lg">
-        <form action="" method="GET">
-          <div>
-            <input name="user" id="user" placeholder="user" />
-            <input name="repo" id="repo" placeholder="repo" />
-            <input name="workflow" id="workflow" placeholder="workflow" />
-            <button>Show</button>
+        <form
+          style={{
+            paddingBottom: '30px',
+            borderBottom: 'solid 1px #ddd',
+            marginBottom: '15px',
+          }}
+        >
+          <div className="form-row">
+            <div class="col">
+              <input name="user" id="user" placeholder="user" className="form-control" />
+            </div>
+            <div class="col">
+              <input name="repo" id="repo" placeholder="repo" className="form-control" />
+            </div>
+            <div class="col">
+              <input
+                name="workflow"
+                id="workflow"
+                placeholder="workflow"
+                className="form-control"
+              />
+            </div>
+            <div class="col">
+              <button className="btn btn-primary" style={{ width: '100%' }}>
+                Show
+              </button>
+            </div>
           </div>
         </form>
         <div id="report"></div>
