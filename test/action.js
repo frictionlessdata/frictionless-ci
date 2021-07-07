e46e6e8e774c72468f06b5f56f8a63e90118faf1
roll <eskarev@gmail.com>
2021-07-07 11:43:09 +0300

    Bootstrapped tests

--- test/action.js
+++ test/action.js
@@ -1,9 +1,34 @@
-import * as config from '../lib/config.js'
+import { dir } from 'tmp-promise'
+import { jest } from '@jest/globals'
+import * as core from '@actions/core'
+import * as artifact from '@actions/artifact'
+import { action } from '../lib/action.js'
 
 // General
 
+let inputs = {}
+
 describe('General', () => {
-  it('main', async () => {
-    expect(config.INQUIRY_PROPERTY).toBe('main')
+  beforeAll(() => {
+    jest.spyOn(core, 'getInput').mockImplementation((name) => inputs[name])
+    jest.spyOn(core, 'setFailed').mockImplementation(jest.fn())
+    jest.spyOn(core, 'error').mockImplementation(jest.fn())
+    jest.spyOn(core, 'warning').mockImplementation(jest.fn())
+    jest.spyOn(core, 'info').mockImplementation(jest.fn())
+    jest.spyOn(core, 'debug').mockImplementation(jest.fn())
+    jest.spyOn(artifact, 'create').mockImplementation(jest.fn())
+  })
+
+  beforeEach(() => {
+    inputs = {}
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('default', async () => {
+    const temp = await dir()
+    console.log(temp.path)
   })
 })
